47af9362db03fc0c4b124a8c6f9528f48c6d73c0
jessehsiu <jesse801122@gmail.com>
2014-11-29 20:47:17 +0800

    One page

--- www/js/index.js
+++ www/js/index.js
@@ -37,14 +37,14 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        console.log("index.js");
+        StatusBar.hide();
         // screen.lockOrientation('landscape');
     },
     // Update DOM on a Received Event
