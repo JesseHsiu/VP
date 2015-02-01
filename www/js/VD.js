9819249d89b0bed9b81512dc4cfe0cbc7c2dbe22
jessehsiu <jesse801122@gmail.com>
2015-02-01 16:12:10 +0800

    VD save file ok

--- www/js/VD.js
+++ www/js/VD.js
@@ -1,41 +1,37 @@
-// document.getElementById('tmp_test').addEventListener('click',this.haha('123'),false);
-var now_q=1;
-next_Qusetion();
+var VD_now_q=1;
+var VD_Answer;
+var VD_time = new Date();
+var VD_starttime;
 
-// var Q_array = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15'];
+
+$('#VP_1').on("pageshow",function(){
+	app.CreateFile("VD");
+	VD_next_Qusetion();
+});
 
 var myArray = ['1.png','2.png','3.png','4.png','5.png']
 
-// console.log(myArray);
-
-
-
-function next_Qusetion() {
-	if (now_q==16)
+function VD_next_Qusetion() {
+	if (VD_now_q==16)
 	{
-		// $.mobile.changePage("Section_endPage.html", "slideup");
 		$.mobile.pageContainer.pagecontainer('change', "Section_endPage.html", {
 		  transition: 'flow'
 		});
 	}
 	else
 	{
-		$("#VP_1_Confirm_btn").css('display','none');
-		// console.log("123");//now_q.toString()
-		//Question
-		$('#VP_1_Q_img').attr('src', 'img/VD/'+now_q.toString()+'/ans/0.png');
-		$('#VP_1_1_img').attr('src', 'img/VD/'+now_q.toString()+'/opt/'+myArray[0]);
-		$('#VP_1_2_img').attr('src', 'img/VD/'+now_q.toString()+'/opt/'+myArray[1]);
-		$('#VP_1_3_img').attr('src', 'img/VD/'+now_q.toString()+'/opt/'+myArray[2]);
-		$('#VP_1_4_img').attr('src', 'img/VD/'+now_q.toString()+'/opt/'+myArray[3]);
-		$('#VP_1_5_img').attr('src', 'img/VD/'+now_q.toString()+'/opt/'+myArray[4]);
-		//add number
-		console.log(now_q);
-		now_q++;
-
-		//disapear
-		
 		myArray.sort(function(){ return Math.random()-0.5; }); 
+		VD_starttime = VD_time.getTime();
+		window.plugins.directoryList.getList("www/img/VD/"+VD_now_q+"/ans",VD_onDirectoryReadSuccess,VD_onDirectoryReadError);
+
+		$("#VP_1_Confirm_btn").css('display','none');
+		
+		//Question
+		$('#VP_1_1_img').attr('src', 'img/VD/'+VD_now_q.toString()+'/opt/'+myArray[0]);
+		$('#VP_1_2_img').attr('src', 'img/VD/'+VD_now_q.toString()+'/opt/'+myArray[1]);
+		$('#VP_1_3_img').attr('src', 'img/VD/'+VD_now_q.toString()+'/opt/'+myArray[2]);
+		$('#VP_1_4_img').attr('src', 'img/VD/'+VD_now_q.toString()+'/opt/'+myArray[3]);
+		$('#VP_1_5_img').attr('src', 'img/VD/'+VD_now_q.toString()+'/opt/'+myArray[4]);
 	}
 }
 
@@ -56,28 +52,29 @@ $('#VP_1_5').on("click", function() {
 	VP_check_option(document.getElementById("VP_1_5_img"));
 });
 
-$('#VP_1_Confirm_btn').on("click", function() {
-	next_Qusetion();
-	// document.getElementById("VP_Confirm_btn").style.display="none";
-});
-
-//$("#id").css('display','block'); 
+// $('#VP_1_Confirm_btn').on("click", function() {
+	
+// 	// document.getElementById("VP_Confirm_btn").style.display="none";
+// });
 
 function VP_check_option (element) {
-	// body...
+
 	console.log(element.src.slice(-5,-4));
-	// $("#VP_1_Next").css('display','block');
-	// $("#VP_Confirm_btn").css('display','block');
-	$("#VP_1_Confirm_btn").css('display','block');
-	// document.getElementById("VP_Confirm_btn").style.display="block";
-	// $("#VP_Confirm_btn").css('display','block');
-	// if (element.src.slice(-5,-4) == '1')
-	// {
-	// 	// $('#VP_1_intro_right').addClass('animated bounceOutLeft');
-		
-	// }
-	// else
-	// {
-	// 	document.getElementById("VP_1_Next").style.display="none";
-	// }
+
+	VD_time= new Date();
+	var Time_tmp = VD_time.getTime() - VD_starttime
+	app.thingstowrite = (VD_Answer === element.src.slice(-5,-4)) +"," + Time_tmp + "," + element.src.slice(-5,-4)+"\n";
+	app.WriteFile();
+	VD_next_Qusetion();
+}
+
+function VD_onDirectoryReadSuccess (directoryList) {
+	VD_Answer = directoryList[0].slice(-5,-4);
+	$('#VP_1_Q_img').attr('src', 'img/VD/'+VD_now_q.toString()+'/ans/'+VD_Answer+'.png');
+
+	VD_now_q++;
+}
+
+function VD_onDirectoryReadError (error) {
+
 }
--- www/js/VP_intro.js
+++ www/js/VP_intro.js
@@ -4,7 +4,7 @@ var myArray = ['1.png','2.png','3.png','4.png','5.png']
 myArray.sort(function(){ return Math.random()-0.5; }); 
 
 //Question
-$('#Q_img').attr('src', 'img/VD/1/ans/0.png');
+$('#Q_img').attr('src', 'img/VD/1/ans/1.png');
 //Options
 $('#1_img').attr('src', 'img/VD/1/opt/'+myArray[0]);
 $('#2_img').attr('src', 'img/VD/1/opt/'+myArray[1]);
